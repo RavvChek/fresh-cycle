27cd87a4d929defffd2b9778227d5a71b8231425
RavvCheck1 <ravilbayan2004@gmail.com>
2025-06-07 23:23:57 +0300

    Initial commit: FreshCycle project setup

---
+++ src/test/java/ru/ravvcheck/laundryApp/LaundryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ravvcheck.laundryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
